a17d0f9f1ef6ed1d6891941328ddbe0bd063aeea
abusiddik95 <siddikabu34@gmail.com>
2020-03-24 16:05:33 -0400

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+// let pakhi;
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return 
+// }
+
+// function add(num1, num2){
+//     console.log(num2)
+// }
+
+const premik = {name:"samrt dude", phone: 654321};
+console.log(premik.name);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,4,6,8];
+console.log(ages[11])
