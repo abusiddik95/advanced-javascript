0d904b207641248019e8e56b7f450dd20c0a3c04
abusiddik95 <siddikabu34@gmail.com>
2020-03-24 12:53:17 -0400

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+// falsy
+// false
+// null
+// NaN
+// '0'
+// Truthy
+// ' '
+// []
+
+
+
+// const age = 4;
+// const name = null
+
+let name = 12;
+console.log(name || name == 0)
+if(name){
+    console.log("condition is true.")
+}else{
+    console.log("condition is false.")
+}
