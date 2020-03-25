07ad6a56c223f78a1d02a61b981f479dc09e7b99
abusiddik95 <siddikabu34@gmail.com>
2020-03-25 11:30:28 -0400

    done

---
+++ arraysplit.js
@@ -0,0 +1,8 @@
+const nums = [1,2,3,4,5,6,7,8,9];
+//const part = nums.slice(2, 5);
+const removed = nums.splice(2, 3);
+//console.log(part);
+console.log(removed);
+
+const together = nums.join (" joined ");
+console.log(together);
---
+++ scope.js
@@ -0,0 +1,15 @@
+let bonus = 20;
+
+function sum(first,second){
+    let result = first + second + bonus;
+    if(result >9 ){
+        const mood = "happy";
+        console.log(mood);
+    }
+    return result;
+    var day = date();
+    console.log(day)
+}
+
+const output = sum(5,7);
+console.log(output);
